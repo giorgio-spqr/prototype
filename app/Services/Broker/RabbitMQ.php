dedb8fe1d2a532d0d1790d8f6ab05a9573608683
giorgio-spqr <61200872+giorgio-spqr@users.noreply.github.com>
2020-08-31 16:28:44 +0300

    Add files via upload

---
+++ app/Services/Broker/RabbitMQ.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Project\Services\Broker;
+
+use Bunny\Client;
+use Bunny\Message;
+use Bunny\Channel;
+use Project\Models\Deals;
+use Project\Utils\AskToDeal;
+use Project\Utils\OfferedDeal;
+
+class RabbitMQ 
+{
+        private static $connection = [
+                'host'      => 'coyote.rmq.cloudamqp.com',
+                'vhost'     => 'zugwmjzp',
+                'user'      => 'zugwmjzp',
+                'password'  => 'ofsDtmr76VEUX8BTVSq35yW3_5sSSOXd',
+            ];
+
+        public static function send($array)
+        {
+            $client = (new Client(self::$connection))->connect();
+            $channel = $client->channel();
+
+            $channel->queueDeclare('email-send', false, true, false, false);
+            $data = json_encode($array);
+            $channel->publish($data, ['delivery-mode' => 2], '', 'email-send');       
+            $channel->close();
+            $client->disconnect();
+        }
+
+        public static function recieve()
+        {
+            $client = (new Client(self::$connection))->connect();
+            $channel = $client->channel();
+            
+            $queue = $channel->queueDeclare('email-send', false, true, false, false);
+            
+            echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+            
+            $channel->qos(0, 5);
+            $channel->run(
+                function (Message $message, Channel $channel, Client $client) {
+                    $data = json_decode($message->content, true);
+                    $deals = new Deals;
+                    
+                    if (!empty($data['offers'])) {
+                        foreach ($data['offers'] as $v) {
+                            echo 'Sending mail to client ' . $v['email'] . PHP_EOL;
+                            OfferedDeal::send('email', $v); // sending email
+                            $ids[] = $v['deal_id'];
+                        }
+
+                        if (count($ids) > 0) {
+                            $deals->updateRows($ids); // updating dt_send in deals table
+                        }
+                    } elseif (!empty($data['partner_id'])) {
+                        echo 'Sending mail to partner ' . $data['email'] . PHP_EOL;
+                        AskToDeal::send('email', $data); // sending email
+                        $deals->create(['applications_id' => $data['application_id'], 'partners_id' => $data['partner_id']]); // adding row to deals with ask flag
+                    }
+                    
+                    $channel->ack($message);
+                },
+                'email-send'
+            );
+        }
+}
