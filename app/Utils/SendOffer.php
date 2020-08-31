6fff81651587163a47dd5e6ffb4b86e71f5efc6a
giorgio-spqr <61200872+giorgio-spqr@users.noreply.github.com>
2020-08-31 16:31:08 +0300

    Add files via upload

---
+++ app/Utils/SendOffer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Project\Utils;
+
+use Project\Models\Deals;
+use Project\Services\Broker\RabbitMQ;
+use Project\Services\Email\SendMail;
+
+interface SendOffer
+{
+    public static function send($type, $data);
+}
+
+class AskToDeal extends Deals implements SendOffer
+{
+    public static function send($type, $data)
+    {
+        if ($type == 'email') {
+            SendMail::send($data);
+        } elseif ($type == 'rabbitmq') {
+            RabbitMQ::send($data);
+        }
+    }
+}
+
+class OfferedDeal extends Deals implements SendOffer
+{
+    public static function send($type, $data)
+    {
+        if ($type == 'email') {
+            SendMail::send($data);
+        } elseif ($type == 'rabbitmq') {
+            RabbitMQ::send($data);
+        }
+    }
+}
