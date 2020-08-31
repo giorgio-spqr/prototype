ee88ab4ef7041b1246206efeac222dda4f8b06ed
giorgio-spqr <61200872+giorgio-spqr@users.noreply.github.com>
2020-08-31 16:30:12 +0300

    Add files via upload

---
+++ app/Services/Email/SendMail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Project\Services\Email;
+
+class SendMail
+{
+    public static function send($data)
+    {
+        // some code for sending e-mail, just for example we write simply to file
+        
+        file_put_contents('mail.txt', $data, FILE_APPEND); // log
+        echo 'Email sent successful!' . PHP_EOL;
+    }
+}
